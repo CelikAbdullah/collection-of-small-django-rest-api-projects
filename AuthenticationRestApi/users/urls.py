7ee5ccf99b200e62b5d3b519f9217607e8640613
CelikAbdullah <abdullah.celik1989@gmail.com>
2021-05-31 14:09:35 +0200

    Added Logout

--- AuthenticationRestApi/users/urls.py
+++ AuthenticationRestApi/users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from users import views
+from knox import views as knox_views
 
 urlpatterns = [
     path('api/auth', include('knox.urls')),
     path('api/auth/register', views.RegisterView.as_view()),
     path('api/auth/login', views.LoginView.as_view()),
     path('changePassword', views.ChangePasswordView.as_view()),
+    path('logout', knox_views.LogoutView.as_view()),
 ]
